6d960b932931debf5f202fbceda18bafb7a01808
Belhavensky44 <Schuleeee123@yandex.ru>
2025-10-09 12:58:29 +0500

    Create moduleReadData.js

---
+++ 2025/lab_2/moduleReadData.js
@@ -0,0 +1,11 @@
+const { readFileSync } = require('fs');
+
+const readDataCsv = (filename) => {
+  return readFileSync(filename, 'utf8')
+    .split(/\r?\n/)
+    .filter(line => line)
+    .slice(1)
+    .map(elm => +elm.split(' ')[1]);
+};
+
+module.exports = { readDataCsv };
